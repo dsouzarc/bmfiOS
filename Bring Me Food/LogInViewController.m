0e65bb852e9eea02fc2e10051671029f5d7b9871
Ryan D'souza <dsouzarc@gmail.com>
2015-02-24 17:12:23 -0500

    Methods to check textfields

--- Bring Me Food/LogInViewController.m
+++ Bring Me Food/LogInViewController.m
@@ -33,7 +33,84 @@
 
 
 - (IBAction)signUpForAccount:(id)sender {
-
     
+    NSString *name = self.userNameTextField.text;
+    NSString *phoneNumber = self.phoneNumberTextField.text;
+    NSString *email = self.emailAddressTextField.text;
+    NSString *password = self.passwordTextField;
+    NSString *confirmedPassword = self.confirmPasswordTextField.text;
+    
+    if(![self validName:name]) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Invalid Field"
+                              message:@"Must enter your full name"
+                              delegate:self cancelButtonTitle:@"Fix it" otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    if(![self validPhoneNumber:phoneNumber]) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Invalid Field"
+                              message:@"Must enter your valid phone number"
+                              delegate:self cancelButtonTitle:@"Fix it" otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    if(![self validEmail:email]) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Invalid Field"
+                              message:@"Must enter your valid email address"
+                              delegate:self cancelButtonTitle:@"Fix it" otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
+    
+    if(![self validPassword:password confirmPassword:confirmedPassword]) {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Invalid Field"
+                              message:@"Passwords do not match"
+                              delegate:self cancelButtonTitle:@"Fix it" otherButtonTitles:nil];
+        [alert show];
+        return;
+    }
 }
+
+- (BOOL) validName:(NSString*) nameString {
+    if(nameString.length < 5) {
+        return NO;
+    }
+    
+    if(![nameString containsString:@" "]) {
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (BOOL) validPhoneNumber:(NSString*) phoneNumber {
+    return YES;
+}
+
+- (BOOL) validEmail:(NSString*) emailString {
+    
+    if([emailString length]==0){
+        return NO;
+    }
+    
+    NSString *regExPattern = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+    
+    NSRegularExpression *regEx = [[NSRegularExpression alloc]
+                                  initWithPattern:regExPattern options:NSRegularExpressionCaseInsensitive error:nil];
+    NSUInteger regExMatches = [regEx numberOfMatchesInString:emailString
+                                                     options:0 range:NSMakeRange(0, [emailString length])];
+    
+    return regExMatches != 0;
+}
+
+- (BOOL) validPassword:(NSString*) password confirmPassword:(NSString*)confirmedPassword {
+    return YES;
+}
+
 @end
