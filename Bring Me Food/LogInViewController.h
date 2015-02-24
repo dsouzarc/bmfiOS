1dbd57e8df2fb4c2e48f4369dd1dc21f4e0950d5
Ryan D'souza <dsouzarc@gmail.com>
2015-02-23 22:01:04 -0500

    First commit + FB/Parse integration

---
+++ Bring Me Food/LogInViewController.h
@@ -0,0 +1,13 @@
+//
+//  LogInViewController.h
+//  Bring Me Food
+//
+//  Created by Ryan D'souza on 2/23/15.
+//  Copyright (c) 2015 Ryan D'souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LogInViewController : UIViewController
+
+@end
