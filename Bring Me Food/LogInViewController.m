c2f7b476618198add386a4dc3ca59045c212115b
Ryan D'souza <dsouzarc@gmail.com>
2015-02-24 16:47:18 -0500

    Added listeners

--- Bring Me Food/LogInViewController.m
+++ Bring Me Food/LogInViewController.m
@@ -9,6 +9,13 @@
 #import "LogInViewController.h"
 
 @interface LogInViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailAddressTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
+
+- (IBAction)signUpForAccount:(id)sender;
 
 @end
 
@@ -24,14 +31,9 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (IBAction)signUpForAccount:(id)sender {
+
+    
 }
-*/
-
 @end
