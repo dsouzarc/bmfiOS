24ca33c53c6e011308e038c62959343ba3bd51d3
Ryan D'souza <dsouzarc@gmail.com>
2015-03-25 16:23:56 -0400

    Successfully displays current orders

--- Bring Me Food/ExistingOrdersViewController.m
+++ Bring Me Food/ExistingOrdersViewController.m
@@ -13,8 +13,10 @@
 @interface ExistingOrdersViewController ()
 
 - (IBAction)createNewOrder:(id)sender;
+- (IBAction)reloadItemsButton:(id)sender;
 
 @property (strong, nonatomic) NSMutableArray *existingOrders;
+@property (strong, nonatomic) IBOutlet UITableView *existingOrdersTableView;
 
 @end
 
@@ -45,8 +47,20 @@
             [self.existingOrders removeAllObjects];
             
             for(NSDictionary *result in results) {
+                Order *order = [[Order alloc] initWithEverything:[result objectForKey:@"restaurantName"]
+                                                 deliveryAddress:[result objectForKey:@"deliveryAddressString"]
+                                                       orderedAt:[[result objectForKey:@"createdAt"]
+                                                                  dateByAddingTimeInterval:-3600*4]
+                                             toBeDeliveredAtTime:[result objectForKey:@"timeToBeDeliveredAt"]
+                                           estimatedDeliveryTime:[result objectForKey:@"estimatedDeliveryTime"]
+                                                      orderItems:[result objectForKey:@"chosenItems"]
+                                                     orderStatus:[[result objectForKey:@"orderStatus"] longValue]];
+                [self.existingOrders addObject:order];
                 
+                NSLog(@"ORDERS: %@", order.restaurantName);
             }
+            
+            [self.existingOrdersTableView reloadData];
             
         }
         
@@ -63,9 +77,29 @@
     // Do any additional setup after loading the view from its nib.
 }
 
+- (NSInteger) numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
 - (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return self.existingOrders.count;
+}
+
+- (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *classifier = @"Cell";
+    
+    UITableViewCell *cell = [self.existingOrdersTableView dequeueReusableCellWithIdentifier:classifier];
+    
+    if(!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:classifier];
+    }
+    
+    cell.textLabel.text = ((RestaurantItem*) self.existingOrders[indexPath.row]).restaurantName;
+    
+    return cell;
 }
 
 - (IBAction)createNewOrder:(id)sender {
@@ -74,5 +108,9 @@
     [self presentViewController:createOrder animated:YES completion:nil];
     
     //self.view.window.rootViewController = createOrder;
+}
+
+- (IBAction)reloadItemsButton:(id)sender {
+    [self updateOrders];
 }
 @end
