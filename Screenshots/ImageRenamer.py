82296a8ad7495f9a3f254a524f2218f70a136ea8
Ryan D'souza <dsouzarc@gmail.com>
2015-04-16 22:24:01 -0400

    Updated to print out Markdown code for displaying screenshots

--- Screenshots/ImageRenamer.py
+++ Screenshots/ImageRenamer.py
@@ -5,15 +5,20 @@ import os;
 #Quickly renames all the photos in a directory with the format
 #'Screenshot_X.png', where X is that file's place in the directory
 
+#Also prints out the markdown code to display these many images
+
 #Run instructions: 'python ImageRenamer.py'
 
 
 files = os.listdir('.');
 index = 0;
 
+prefix = "https://github.com/dsouzarc/bmfiOS/blob/master/Screenshots/";
+
 for fileName in files:
 
     if fileName != "ImageRenamer.py":
-        print(fileName);
-        os.rename(fileName, "Screenshot_" + str(index) + ".png");
+        newFileName = "Screenshot_" + str(index) + ".png";
+        os.rename(fileName, newFileName);
+        print("![Screenshot " + str(index) + "](" + prefix + newFileName + ")");
         index += 1;
