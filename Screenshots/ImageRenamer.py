d7b62986b3de904609599d83d5e642d1b4044e3b
Ryan D'souza <dsouzarc@gmail.com>
2015-04-16 22:01:58 -0400

    Comment

--- Screenshots/ImageRenamer.py
+++ Screenshots/ImageRenamer.py
@@ -1,5 +1,13 @@
 import os;
 
+
+#Written by Ryan Dsouza
+#Quickly renames all the photos in a directory with the format
+#'Screenshot_X.png', where X is that file's place in the directory
+
+#Run instructions: 'python ImageRenamer.py'
+
+
 files = os.listdir('.');
 index = 0;
 
